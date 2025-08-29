3dc716fbae4bf01278131f7a50d22a436817a3d1
davidtmscott <davidtmscott@gmail.com>
2025-08-29 09:44:46 +0100

    Add sierpinski triangle fractal

---
+++ scripts/sierpinski_triangle.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+
+# function to create Sierpinski triangle (gasket) through iterations of a simple rule
+
+chaos_game <- function(){
+  # create data frame of three initial points
+  #df <- data.frame(letter = c("A", "B", "C"), x = c(0, 3, 4), y = c(0, 4, 1))
+  df <- data.frame(letter = c("A", "B", "C"), x = c(0, 2, 4), y = c(0, 4, 0))
+  # plot points on a graph
+  plot(df$x, df$y, xlab = "X", ylab = "Y")
+  # initialize point vector X
+  Xpoint = xy.coords(0,0)
+  # plot the point as an "x"
+  points(Xpoint, pch = 4, cex = 0.5)
+  for (i in 1:10000){
+    # randomly sample three points in data frame (A, B, C)
+    rowsamp <- df[sample(nrow(df), 1), ]
+    # one point x halfway towards the sampled point
+    x <- (rowsamp$x + Xpoint$x) / 2
+    y <- (rowsamp$y + Xpoint$y) / 2
+    # update location of X point
+    Xpoint <- xy.coords(x, y)
+    # replot in new location
+    points(Xpoint, pch = 4, cex = 0.5)
+  }
+}
+
+chaos_game()
